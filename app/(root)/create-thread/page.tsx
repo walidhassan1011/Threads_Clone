4b4b3ce84dde46177b841d4af8f300096554851d
walidhassan1011 <walidhassan009@gmail.com>
2023-09-07 19:59:09 +0300

    test deploy

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -15,7 +15,7 @@ async function Page() {
   return (
     <>
       <h1 className="head-text">Create Thread</h1>
-      <PostThread userId={userInfo._id} />
+      <PostThread userId={userInfo?._id} />
     </>
   );
 }
